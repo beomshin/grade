54c8d8a8888d0a4ccc9a0f58b2d73a834949947d
beomshin.kim <chzh859@naver.com>
2025-11-18 22:35:57 +0900

    # 프로젝트 생성

---
+++ src/main/java/com/kr/grade/GradeCollectApplication.java
@@ -0,0 +1,13 @@
+package com.kr.grade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradeCollectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradeCollectApplication.class, args);
+    }
+
+}
